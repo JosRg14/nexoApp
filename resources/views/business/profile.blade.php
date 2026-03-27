3b31573a611bdd854f14991e46541728a7c5f424
Josué Ginez <ginezivan409@gmail.com>
2026-03-27 17:52:38 -0600

    se quitó la s flotante

--- resources/views/business/profile.blade.php
+++ resources/views/business/profile.blade.php
@@ -117,7 +117,7 @@ class="w-full text-left px-4 py-2 text-xs text-red-400 hover:text-red-300 hover:
         <div class="relative min-h-[600px]">
             @include('business.profile.information')
             @include('business.profile.services')
-            @include('business.profile.schedule')s
+            @include('business.profile.schedule')
             @include('business.profile.finances')
             @include('business.profile.staff')
         </div>
@@ -368,11 +368,7 @@ function showToast(message) {
         }, 300);
     }, 3000);
 }
-
-
     </script>
-
-
 
 </body>
 </html>
