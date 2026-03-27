04a93cb339fec9830f2d5bc2970d7b91317788c3
Josué Ginez <ginezivan409@gmail.com>
2026-03-26 18:22:36 -0600

    debug en profile blade

--- resources/views/business/profile.blade.php
+++ resources/views/business/profile.blade.php
@@ -113,6 +113,10 @@ class="w-full text-left px-4 py-2 text-xs text-red-400 hover:text-red-300 hover:
         <!-- Content Area -->
         <div class="relative min-h-[600px]">
             @include('business.profile.information')
+            <!-- DEBUG: Verificar que $services llega -->
+<pre style="background: #000; color: #0f0; padding: 10px; margin: 10px; font-size: 10px; overflow: auto;">
+SERVICIOS: {{ json_encode($services, JSON_PRETTY_PRINT) }}
+</pre>
             @include('business.profile.services')
             @include('business.profile.finances')
             @include('business.profile.staff')
